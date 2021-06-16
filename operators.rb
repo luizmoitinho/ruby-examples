f5103951c562fbf1b0e67e843130b62b8f5924fe
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-06-15 22:36:24 -0300

    feat: operadores matematicos

---
+++ operators.rb
@@ -0,0 +1,22 @@
+puts "Operadores"
+
+# + | - | * | / | % | **
+
+
+puts "\n10 + 10"
+puts 10 + 10
+
+puts "\n10 - 5"
+puts 10 - 5
+
+puts "\n10 * 10"
+puts 10 * 10
+
+puts "\n10 / 2"
+puts 10 / 2
+
+puts "\n10 % 2"
+puts 10 % 2
+
+puts "\n10 ** 2"
+puts 10 ** 2
