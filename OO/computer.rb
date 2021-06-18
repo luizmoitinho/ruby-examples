ba627025352ab826f165c03fe94e3ed8bc44f2c9
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-06-18 14:37:54 -0300

    feat: criando uma classe computer e dando comportamento

---
+++ OO/computer.rb
@@ -0,0 +1,16 @@
+class Computer
+
+  def turn_on
+    'turn on the computer'
+  end
+
+  def shutdown
+    'shutdown the computer'
+  end
+
+end
+
+ my_computer = Computer.new
+
+ puts my_computer.turn_on
+ puts my_computer.shutdown
